3b6149875aa5ffed34642260b6e04076401f4f4f
mohitkokcha203 <130283675+mohitkokcha203@users.noreply.github.com>
2023-04-09 15:43:00 +0530

    w3schoolclone

---
+++ w3 school clone/javascript.js
@@ -0,0 +1,11 @@
+function changeimage()
+{
+    var img=document.getElementById('image');
+    img.src='best2.gif';
+}
+function changeimg()
+{
+    var img=document.getElementById('image');
+    img.src='codeeditor.gif'; 
+    
+}
